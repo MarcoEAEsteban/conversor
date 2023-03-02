030df322618f6d5c0d959302bcfbfecaeb64ce4b
Marco Aucaruri <104728524+MarcoEAEsteban@users.noreply.github.com>
2023-03-02 16:05:04 -0300

    mejoras en conversor
    adicion de selectores con js.

--- index.js
+++ index.js
@@ -1,40 +1,65 @@
-
-
-let res = document.getElementById('res')
 let seleccion = document.getElementById('moneda')
+let res = document.getElementById('res')
+let nombre = document.getElementById('nombre')
+let cantidad = document.getElementById('monto')
+let tempera = document.getElementById('temperatura')
+
+let opSol = document.createElement('option')
+let opDolar = document.createElement('option')
+opSol.innerHTML = 'soles'
+opDolar.innerHTML = 'dolar'
+seleccion.appendChild(opSol)
+seleccion.appendChild(opDolar)
+
 let seleccionar = document.getElementById('tem')
+let opCel = document.createElement('option')
+let opFar = document.createElement('option')
+opCel.innerHTML = '°C'
+opFar.innerHTML = '°F'
+seleccionar.appendChild(opCel)
+seleccionar.appendChild(opFar)
+
 let boton = document.getElementById('convertir')
 boton.addEventListener('click', crear)
+
 function crear(){  
     let conversor = document.getElementsByName('conver')
-    if(conversor[0].checked){
-        
-        let cantidad = document.getElementById('monto')
-        let dinero = Number(cantidad.value)
-        var lenguajes = seleccion.options[seleccion.selectedIndex]
-        var lengua = lenguajes.value
-        if (lengua == 'soles'){
-            res.innerHTML = `tipo de cambio: s/${dinero*0.73}`
-        }    
-        if (lengua == "dolar"){
-            res.innerHTML = `tipo de cambio: $${dinero*0.19}`
-        } 
-           
-        
+    let usuario = nombre.value
+    if(nombre.value == 0 || monto.value == 0 && temperatura.value == 0){
+        alert('Rellene los espacios faltantes')
     }else{
-        if (conversor[1].checked){
-            let tempera = document.getElementById('temperatura')
-            let tem = Number(tempera.value)
-            let temperatura = seleccionar.options[seleccionar.selectedIndex]
-            let tempe = temperatura.value
-            if(tempe =='°F'){
-                res.innerHTML = `${( tem * 9/5)+32}`
+        if(conversor[0].checked){
+        
+            
+            let dinero = Number(cantidad.value)
+            var lenguajes = seleccion.options[seleccion.selectedIndex]
+            var lengua = lenguajes.value
+            
+            if (lengua == 'soles'){
+                res.innerHTML = `${usuario}, el tipo de cambio es: s/${dinero*0.73}`
+            }    
+            if (lengua == "dolar"){
+                res.innerHTML = `${usuario}, el tipo de cambio es: $${dinero*0.19}`
+            } 
+             
+            
+        }else{
+            if (conversor[1].checked){
                 
-            }
-            if(tempe == '°C'){
-                res.innerHTML = `${(tem -32)* 5/9}`
+                let tem = Number(tempera.value)
+                let temperatura = seleccionar.options[seleccionar.selectedIndex]
+                let tempe = temperatura.value
+                if(tempe =='°F'){
+                    res.innerHTML = `${usuario} la temperatura es: °${( tem * 9/5)+32}`
+                    
+                }
+                if(tempe == '°C'){
+                    res.innerHTML = `${usuario} la temperatura es: °${(tem -32)* 5/9}`
+                }
             }
         }
+        formulario.reset()
+
     }
-    formulario.reset()
+    
 }
